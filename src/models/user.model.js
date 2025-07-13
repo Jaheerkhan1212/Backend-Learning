da7153fe9535ea537d7863e2b1620447f61e4bf6
Jaheer <zaheerorjaheer@gmail.com>
2025-07-13 23:21:09 +0530

    jwt generate token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose,{Schema} from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema=new Schema({
     username:{
         type:String,
@@ -47,6 +48,23 @@ if(!this.isModified("password"))return next();
 this.password= await bcrypt(this.password,10);
 next();
 })
+userSchema.methods.generateAccessToken=function(){
+   return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        fullName:this.fullName,
+        username:this.username
+    },process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+};
+userSchema.methods.generateRefreshToken=function(){
+     return jwt.sign({
+        _id:this._id,
+    },process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+};
 userSchema.methods.isPasswordCorrect=async function(password) {
     return await bcrypt.compare(password,this.password);
 }
